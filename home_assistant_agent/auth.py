8ede43a6d07aed8a334208604a57c5aa4ab73cce
KnucklesSG1 <knucklessg1@gmail.com>
2026-04-01 01:44:42 -0500

    Initial commit.

--- home_assistant_agent/auth.py
+++ home_assistant_agent/auth.py
@@ -7,6 +7,7 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 from home_assistant_agent.home_assistant_api import HomeAssistantApi
+from agent_utilities.exceptions import AuthError, UnauthorizedError
 
 _client = None
 
